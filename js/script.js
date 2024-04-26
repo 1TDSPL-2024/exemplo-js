9297a882794770f0fe4f8246b55f0b91121d8fa6
arthvm <90999445+arthvm@users.noreply.github.com>
2024-04-26 12:06:39 -0300

    Add random color background on button click

--- js/script.js
+++ js/script.js
@@ -4,8 +4,22 @@ let shrekButton = document.querySelector(".shrek-btn");
 console.log("Hello World!");
 console.log(headerTitle.textContent)
 
-shrekButton.addEventListener("click", () =>{
-    alert(`
+function getRandomInt(max){
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function getRandomColor(){
+    let color = "#";
+    colorIndex = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+    for(i=1; i<= 6; i++){
+        color += colorIndex[getRandomInt(15)];
+    }
+
+    return color;
+}
+
+shrekButton.addEventListener("click", function(){
+    console.log(`
 
     ⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
     ⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
@@ -24,4 +38,6 @@ shrekButton.addEventListener("click", () =>{
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉
     
     `);
+    document.body.style.backgroundColor = getRandomColor();
+
 });
