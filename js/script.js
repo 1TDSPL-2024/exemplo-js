2a81429161bb19a4f3a72015169a62649121eb0f
arthvm <90999445+arthvm@users.noreply.github.com>
2024-04-30 11:13:19 -0300

    Solve basic JS practice exercises

--- js/script.js
+++ js/script.js
@@ -2,24 +2,41 @@ let headerTitle = document.querySelector(".header--title");
 let shrekButton = document.querySelector(".shrek-btn");
 
 console.log("Hello World!");
-console.log(headerTitle.textContent)
+console.log(headerTitle.textContent);
 
-function getRandomInt(max){
-    return Math.floor(Math.random() * (max + 1));
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
 
-function getRandomColor(){
-    let color = "#";
-    colorIndex = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
-    for(i=1; i<= 6; i++){
-        color += colorIndex[getRandomInt(15)];
-    }
+function getRandomColor() {
+  let color = "#";
+  colorIndex = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+  for (i = 1; i <= 6; i++) {
+    color += colorIndex[getRandomInt(15)];
+  }
 
-    return color;
+  return color;
 }
 
-shrekButton.addEventListener("click", function(){
-    console.log(`
+shrekButton.addEventListener("click", function () {
+  console.log(`
 
     ⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
     ⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
@@ -38,6 +55,43 @@ shrekButton.addEventListener("click", function(){
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉
     
     `);
-    document.body.style.backgroundColor = getRandomColor();
-
+  document.body.style.backgroundColor = getRandomColor();
 });
+
+// EXEC 1
+var x = "var";
+let y = "let";
+const z = "CONST";
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+// EXEC 2
+let num = 1;
+let str = "String";
+let bool = true;
+let arr = [1, "2", true];
+let obj = {
+  prop1: 14,
+  prop2: "Property 2",
+  prop3: false,
+  prop4: [1, 2, 3],
+};
+console.log(num);
+console.log(str);
+console.log(bool);
+console.log(arr);
+console.log(obj);
+
+// EXEC 3
+let string = "25";
+console.log(Number(string));
+
+// EXEC 4
+let number = 30;
+console.log(number.toString());
+
+// EXEC 5
+let boolean = 0;
+console.log(Boolean(boolean));
