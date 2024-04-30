bb22be523e0a92cc2c710675115a9d1061686f36
guimaggiorini <guihm929@gmail.com>
2024-04-30 11:03:17 -0300

    Resolução dos exercícios

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// EXERCÍCIOS
+
+console.log("Exercício 1")
+// Exercício 1 - Declarando Variáveis:
+// Declare três variáveis diferentes (x, y, z) usando var, let e const, respectivamente. Em seguida, imprima o valor de cada variável no console.
+var x = 1
+let y = 2
+const z = 3
+console.log(x)
+console.log(y)
+console.log(z)
+
+console.log("Exercício 2")
+// Exercício 2 - Tipos de Dados:
+// Declare variáveis representando diferentes tipos de dados (número, string, booleano, array e objeto) e imprima cada uma delas no console.
+let exnumero = 10
+let exstring = "Olá, mundo!"
+let exboolean = true
+let exarray = [1, 2, 3, 4, 5]
+let exobjeto = {
+    nome: "Guilherme",
+    rm: "554745"
+}
+console.log(exnumero)
+console.log(exstring)
+console.log(exboolean)
+console.log(exarray)
+console.log(exobjeto)
+
+console.log("Exercício 3")
+// Exercício 3 - Conversão de String para Número:
+// Converta a string "25" em um número e imprima no console.
+let exconverter = parseInt("25")
+console.log(exconverter)
+
+console.log("Exercício 4")
+// Exercício 4 - Conversão de Número para String:
+// Converta o número 30 em uma string e imprima no console.
+let exconverter2num = 30
+let exconverter2 = exconverter2num.toString()
+console.log(exconverter2)
+
+console.log("Exercício 5")
+// Exercício 5 - Conversão para Booleano:
+// Converta o valor 0 para um booleano e imprima no console.
+let exconverter3 = Boolean(0)
+console.log(exconverter3)
