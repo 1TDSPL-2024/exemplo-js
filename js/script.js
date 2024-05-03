900d266787bcfeccf1221fc9eaf57f483d26149f
iannrb <ian007953@gmail.com>
2024-05-03 11:54:02 -0300

    Validação de formulários

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-// "use strict"
-// //Hoisting
-// // var nome = "Joaquim";
+"use strict"
+//Hoisting
+// var nome = "Joaquim";
 
-// // if(true)
-// // {
-// //     let nome = "João";
-// // }
+// if(true)
+// {
+//     let nome = "João";
+// }
 
-// // console.log(nome);
+// console.log(nome);
 
-// // Declaração de variáveis (var, let, const):
-// // Usando var
-// // var x = 10;
-// // console.log(x);
+// Declaração de variáveis (var, let, const):
+// Usando var
+// var x = 10;
+// console.log(x);
 
-// // // Usando let
-// // let y = 20;
-// // console.log(y);
+// // Usando let
+// let y = 20;
+// console.log(y);
 
-// // // Usando const (constante, valor não pode ser alterado)
-// // const z = 30;
-// // console.log(z);
-// // // z = 20;
+// // Usando const (constante, valor não pode ser alterado)
+// const z = 30;
+// console.log(z);
+// // z = 20;
 
-// // Tipos de dados em JavaScript:
+// Tipos de dados em JavaScript:
 
 // // Number
 // let num = 10;
@@ -94,73 +94,124 @@
 // let booleano = Boolean(valor);
 // console.log(booleano); // Saída: false (0 é considerado false)
 
-// // EXERCÍCIOS
+// EXERCÍCIOS
 
-// // Exercício 1 - Declarando Variáveis:
-// // Declare três variáveis diferentes (x, y, z) usando var, let e const, respectivamente. Em seguida, imprima o valor de cada variável no console.
+// Exercício 1 - Declarando Variáveis:
+// Declare três variáveis diferentes (x, y, z) usando var, let e const, respectivamente. Em seguida, imprima o valor de cada variável no console.
+// var x = 10;
+// let y = 20;
+// const z = 30;
 
-// var x = 1
-// console.log(x)
-
-// let y = 2
-// console.log(y)
-
-// const z = 3
-// console.log(z)
+// console.log(x);
+// console.log(y);
+// console.log(z);
 
 // // Exercício 2 - Tipos de Dados:
 // // Declare variáveis representando diferentes tipos de dados (número, string, booleano, array e objeto) e imprima cada uma delas no console.
-
-// let numexec = 7
-// console.log(numexec)
-
-// let string = "Exercício 2"
-// console.log(string)
-
-// let boolean = false
-// console.log(boolean)
-
-// let array = [1, 2, 3, 4, 5]
-// console.log(array)
-
-// let objeto = {professor: "Ale Carlos", matéria: "Front-end"}
-// console.log(objeto)
+// let num = 10;
+// let str = "string";
+// let bool = true;
+// let arr = [1, 2, 3];
+// let obj = {nome : "João", idade: 25};
+// console.log(num);
+// console.log(str);
+// console.log(bool);
+// console.log(arr);
+// console.log(obj);
 
 // // Exercício 3 - Conversão de String para Número:
 // // Converta a string "25" em um número e imprima no console.
-
-// let stringexec = "25"
-// let stringtonum = parseInt(stringexec)
-// console.log(stringtonum)
+// let strNumero = "25.5";
+// let numero = parseFloat(strNumero);
+// console.log(numero);
 
 // // Exercício 4 - Conversão de Número para String:
 // // Converta o número 30 em uma string e imprima no console.
-
-// let converter = 30
-// let numtostring = converter.toString()
-// console.log(numtostring)
+// let numeroStr = 30;
+// console.log(numeroStr.toString());
 
 // // Exercício 5 - Conversão para Booleano:
 // // Converta o valor 0 para um booleano e imprima no console.
+// let valor = 1;
+// let booleano = Boolean(valor);
+// console.log(booleano);
 
-// let value = 0
-// let boleanexec = Boolean(value)
-// console.log(boleanexec)
+//MANIPULAÇÃO DO DOM
+//Funçoes
 
-// MANIPULAÇÃO DO DOM
-// Funções
+//Declarando funções clássica
+// function soma(a, b) {
+//   return a + b;
+// }
+// //Declarando funções anônimas
+//  let soma1 = function(a, b) {
+//    return a + b;
+//  }
 
-// Declarando funções clássica
-function soma(a, b) {
-    return a + b
-}
+// //Recuperando o valor da função através de variável e imprimindo!
+// let resultado = soma(10, 20);
+// console.log(resultado);
+// console.log(soma1(2,2));
 
-// Declarando funções anônimas
-let soma1 = function(a, b) {
-    return a + b
-} 
 
-// Recuperando o valor da função atráves de uma variável
-let resultado = soma(10, 20)
-console.log(resultado)
-console.log(soma1(2, 2))
+//Criar uma lista de usuários
+
+let listaUsuarios = [
+  {emailUsuario:"jose@email.com",senhaUsuario:"123456"},
+  {emailUsuario:"joaquim@email.com",senhaUsuario:"123456"},
+  {emailUsuario:"maria@email.com",senhaUsuario:"123456"},
+  {emailUsuario:"manoel@email.com",senhaUsuario:"123456"},
+  {emailUsuario:"roberto@email.com",senhaUsuario:"123456"},
+];
+// let usuario = {
+//   email:"jose@email.com",
+//   senha:"senha"
+// }
+
+// for (let x = 0; x < listaUsuarios.length; x++) {
+//   if(u.emailUsuario === usuario.email){
+//     console.log("Email Encontrado!");
+//   }
+// }
+
+const btnSubmit = document.querySelector("button[type=submit]");
+//Adicionando um evento ao botão submit.
+btnSubmit.addEventListener("click",function(){
+  //Recuperando os dados dos inputs dos usuários:
+  let email = document.querySelector("input[type=email]").value;
+  let senha = document.querySelector("input[type=password]").value;
+
+  //Criando o objeto que vai guardar os dados que será digitado no form.
+  let usuario = {
+    email : "",
+    senha : ""
+  }
+
+  //Populando o objeto com os dados do form.
+  usuario.email = email;
+  usuario.senha = senha;
+
+  const msg = document.querySelector(".valida");
+
+  //Criando sistema de validação com loop.
+  for (let x = 0; x < listaUsuarios.length; x++) {
+
+    if((usuario.email === listaUsuarios[x].emailUsuario) && (usuario.senha === listaUsuarios[x].senhaUsuario)){
+      
+      msg.textContent = "Login validado com sucesso!";
+      msg.setAttribute("class","sucess");
+      setTimeout(()=>{
+        msg.setAttribute("class","valida");
+      }, 5000);
+      return true;
+    }
+
+  }
+
+  msg.textContent = "Login ou senha inválidos!";
+  msg.setAttribute("class","error");
+  setTimeout(()=>{
+    msg.setAttribute("class","valida");
+  }, 5000);
+  
+})
