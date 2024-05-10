f4950bf84fa96f51e4f5504c08b5502e9805433e
ThomazBartol <thomaz.bartol2006@gmail.com>
2024-05-10 11:35:32 -0300

    Finalizando o login com session e localStorage

--- js/script-sucesso.js
+++ js/script-sucesso.js
@@ -1,18 +1,19 @@
+//Recuperando o token do usuário no sessionStorage
+let tokenUser = sessionStorage.getItem("token");
 
+if(tokenUser != "" && tokenUser != undefined ){
 
 //Recuperando o objeto do usuário logado.
-const usuario = JSON.parse(localStorage.getItem("usuario-logado"));
-
-if(usuario){
-
+    const usuario = JSON.parse(localStorage.getItem("usuario-logado"));
     const elWelcomeUser = document.querySelector("#user-welcome");
     elWelcomeUser.innerText = usuario.email;
 
     const elLogoutUser = document.querySelector("#user-logout");
     elLogoutUser.addEventListener("click", ()=>{
+        sessionStorage.removeItem("token");
         localStorage.removeItem("usuario-logado");
         window.location.href = "../index.html";
     });
 }else{
-    window.location.href = "../index.html";
-}  
+    window.location.href = "../status/erro.html";
+}   
